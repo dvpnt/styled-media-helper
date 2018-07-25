7f078c0b2f986f4b7506de73ff3b2bd4e8795a3b
shtyr-maxim <sh.maxim97@gmail.com>
2018-07-25 13:52:36 +0300

    rewrite tests to tap

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
-const assert = require('assert');
+const t = require('tap');
+const Media = require('../dist');
 const {
 	BreakpointNotFoundError, NextBreakpointNotFoundError
-} = require('../src/errors');
-const Media = require('../src');
+} = require('../dist/errors');
 
-describe('Media class', () => {
+t.test('Media class', (t) => {
 	const sizes = {
 		xs: 20,
 		lg: 100
@@ -12,153 +12,96 @@ describe('Media class', () => {
 
 	const media = new Media(sizes);
 
-	describe('isBreakpoint', () => {
-		describe('with wrong breakpoint', () => {
-			it('check result', () => {
-				assert.strictEqual(media._isBreakpoint('wrong'), false);
-			});
-		});
-
-		describe('success', () => {
-			it('check result', () => {
-				assert.strictEqual(media._isBreakpoint('xs'), true);
-			});
-		});
+	t.test('isBreakpoint', (t) => {
+		t.is(media._isBreakpoint('wrong'), false, 'wrong breakpoint');
+		t.is(media._isBreakpoint('xs'), true, 'right breakpoint');
+		t.end();
 	});
 
-	describe('next', () => {
-		describe('no next breakpoint', () => {
-			it('check result', () => {
-				assert.strictEqual(media._next('lg'), undefined);
-			});
-		});
-
-		describe('success', () => {
-			it('check result', () => {
-				assert.strictEqual(media._next('xs'), 'lg');
-			});
-		});
+	t.test('next', (t) => {
+		t.is(media._next('lg'), undefined, 'has no next breakpoint');
+		t.is(media._next('xs'), 'lg', 'has next breakpoint');
+		t.end();
 	});
 
-	describe('up', () => {
-		describe('with wrong breakpoint', () => {
-			it('check error', () => {
-				assert.throws(() => {
-					media.up('wrong')`color: blue;`;
-				}, BreakpointNotFoundError);
-			});
-		});
-
-		describe('success', () => {
-			it('check result', () => {
-				assert.deepStrictEqual(media.up('xs')`color: blue;`, [
-					'@media (min-width:',
-					'20',
-					'px){',
-					'color: blue;',
-					'}'
-				]);
-			});
-		});
+	t.test('up', (t) => {
+		t.throws(
+			media.up('wrong'),
+			new BreakpointNotFoundError('wrong'),
+			'wrong breakpoint'
+		);
+		t.same(
+			media.up('xs')`color: blue;`,
+			['@media (min-width:', '20', 'px){', 'color: blue;', '}'],
+			'right breakpoint'
+		);
+		t.end();
 	});
 
-	describe('down', () => {
-		describe('with wrong breakpoint', () => {
-			it('check error', () => {
-				assert.throws(() => {
-					media.down('wrong')`color: blue;`;
-				}, BreakpointNotFoundError);
-			});
-		});
-
-		describe('with no next breakpoint', () => {
-			it('check error', () => {
-				assert.throws(() => {
-					media.down('lg')`color: blue;`;
-				}, NextBreakpointNotFoundError);
-			});
-		});
-
-		describe('success', () => {
-			it('check result', () => {
-				assert.deepStrictEqual(media.down('xs')`color: blue;`, [
-					'@media (max-width:',
-					'99.98',
-					'px){',
-					'color: blue;',
-					'}'
-				]);
-			});
-		});
+	t.test('down', (t) => {
+		t.throws(
+			media.down('wrong'),
+			new BreakpointNotFoundError('wrong'),
+			'wrong breakpoint'
+		);
+		t.throws(
+			media.down('lg'),
+			new NextBreakpointNotFoundError('lg'),
+			'has no next breakpoint'
+		);
+		t.same(
+			media.down('xs')`color: blue;`,
+			['@media (max-width:', '99.98', 'px){', 'color: blue;', '}'],
+			'right breakpoint'
+		);
+		t.end();
 	});
 
-	describe('between', () => {
-		describe('with wrong min breakpoint', () => {
-			it('check error', () => {
-				assert.throws(() => {
-					media.between('wrong', 'lg')`color: blue;`;
-				}, BreakpointNotFoundError);
-			});
-		});
-
-		describe('with wrong max breakpoint', () => {
-			it('check error', () => {
-				assert.throws(() => {
-					media.between('xs', 'wrong')`color: blue;`;
-				}, BreakpointNotFoundError);
-			});
-		});
-
-		describe('success', () => {
-			it('check result', () => {
-				assert.deepStrictEqual(media.between('xs', 'lg')`color: blue;`, [
-					'@media ( min-width:',
-					'20',
-					'px ) and ( max-width:',
-					'99.98',
-					'px ){',
-					'color: blue;',
-					'}'
-				]);
-			});
-		});
+	t.test('between', (t) => {
+		t.throws(
+			media.between('wrong min', 'lg'),
+			new BreakpointNotFoundError('wrong min'),
+			'wrong min breakpoint'
+		);
+		t.throws(
+			media.between('lg', 'wrong max'),
+			new BreakpointNotFoundError('wrong max'),
+			'wrong max breakpoint'
+		);
+		t.same(
+			media.between('xs', 'lg')`color: blue;`,
+			[
+				'@media ( min-width:', '20', 'px ) and ( max-width:', '99.98', 'px ){',
+				'color: blue;', '}'
+			],
+			'right breakpoints'
+		);
+		t.end();
 	});
 
-	describe('only', () => {
-		describe('with wrong breakpoint', () => {
-			it('check error', () => {
-				assert.throws(() => {
-					media.only('wrong')`color: blue;`;
-				}, BreakpointNotFoundError);
-			});
-		});
+	t.test('only', (t) => {
+		t.throws(
+			() => media.only('wrong'),
+			new BreakpointNotFoundError('wrong'),
+			'wrong breakpoint'
+		);
+		t.test('success', (t) => {
+			t.same(
+				media.only('lg')`color: blue;`,
+				['@media (min-width:', '100', 'px){', 'color: blue;', '}'],
+				'without next breakpoint'
+			);
 
-		describe('success', () => {
-			describe('without next breakpoint', () => {
-				it('check result', () => {
-					assert.deepStrictEqual(media.only('lg')`color: blue;`, [
-						'@media (min-width:',
-						'100',
-						'px){',
-						'color: blue;',
-						'}'
-					]);
-				});
-			});
-
-			describe('with next breakpoint', () => {
-				it('check result', () => {
-					assert.deepStrictEqual(media.only('xs')`color: blue;`, [
-						'@media ( min-width:',
-						'20',
-						'px ) and ( max-width:',
-						'99.98',
-						'px ){',
-						'color: blue;',
-						'}'
-					]);
-				});
-			});
+			t.same(
+				media.only('xs')`color: blue;`,
+				['@media ( min-width:', '20', 'px ) and ( max-width:', '99.98', 'px ){',
+					'color: blue;', '}'
+				],
+				'with next breakpoint'
+			);
+			t.end();
 		});
+		t.end();
 	});
+	t.end();
 });
